7704741e2eddfd3ed43539276872e13939e68df1
Nicolas Morange <nicolas.morange@cern.ch>
2016-01-26 23:00:34 +0100

    Progress on automated tests

--- CI/build_and_test.sh
+++ CI/build_and_test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-RESULTSDIR=/afs/cern.ch/work/n/nmorange/public/CI
+RESULTSDIR=/afs/cern.ch/work/n/nmorange/public/CI/results
+PLOTSDIR=/afs/cern.ch/user/n/nmorange/www/HbbPlots/CI/
+SAMPLE=/afs/cern.ch/work/n/nmorange/public/CI/sample
+
+mkdir -vp ${RESULTSDIR}
+mkdir -vp ${PLOTSDIR}
 
 TODAY=$(date +%y-%m-%d-%H-%M)
 if [ -z $1 ]
@@ -11,10 +16,32 @@ else
 fi
 echo "Testing against $AGAINST"
 
+mkdir -vp $RESULTSDIR/$TODAY
+
 cd /tmp/nmorange
 mkdir CxAOD
 cd CxAOD
 svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/HiggsPhys/Run2/Hbb/CxAODFramework/FrameworkSub/trunk FrameworkSub
 source FrameworkSub/bootstrap/setup-trunk.sh
 rc build
+
+for deriv in HIGG5D1 HIGG5D2 HIGG2D4
+do
+
+  # need to hack the config file to replace the selectionName :-((((
+  if [ $deriv = "HIGG5D1" ]
+  then
+    selName=0lep
+  elif [ $deriv = "HIGG5D2" ]
+    selName=1lep
+  else
+    selName=2lep
+  fi
+  sed '/string selectionName/c\string selectionName = ${selName}' FrameworkExe/data/framework-run.cfg > config_${deriv}.cfg
+
+  hsg5framework --sampleIn $SAMPLE/${deriv}/ --submitDir $RESULTSDIR/$TODAY/${deriv} --config config_${deriv}.cfg
+  mkdir -vp ${PLOTSDIR}/${TODAY}/${deriv}
+  root -b -l -q "FrameworkExe/validation/CompareCxAODs.C(\"${RESULTSDIR}/${AGAINST}/${deriv}/data-CxAOD/CxAOD.root\", \"${RESULTSDIR}/${TODAY}/${deriv}/data-CxAOD/CxAOD.root\", \"${PLOTSDIR}/${TODAY}/${deriv}\")"
+
+done
 
